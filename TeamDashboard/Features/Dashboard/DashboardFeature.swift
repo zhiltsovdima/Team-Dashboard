3dfd67fc4d9a11e6b56a169ad98879e023c1c3dd
zhiltsovdima <zhiltsovdima@gmail.com>
2025-03-17 22:41:42 +0300

    feat: main features init

---
+++ TeamDashboard/Features/Dashboard/DashboardFeature.swift
@@ -0,0 +1,22 @@
+//
+//  DashboardFeature.swift
+//  TeamDashboard
+//
+//  Created by Dima Zhiltsov on 17.03.2025.
+//
+
+import ComposableArchitecture
+import Foundation
+
+@Reducer
+struct DashboardFeature {
+    
+    struct State: Equatable {
+        
+    }
+    
+    enum Action {
+        
+    }
+    
+}
